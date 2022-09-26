ff035e41141eb265cac382b0db761aa010bf66fd
RobertaRizzuto <rtarizzuto@gmail.com>
2022-09-26 15:47:03 +0200

    feature:first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.App}>
+   <h1 className={styles.title}>Project West</h1>
+   <div className={styles.color1} ></div>
+   <div className={styles.color2} ></div>
+   <div className={styles.color3} ></div>
+   <div className={styles.color4} ></div>
+   <div className={styles.color5} ></div>
+   <div className={styles.color6} ></div>
+   <div className={styles.color7} ></div>
+
+    </div>
+  );
+}
+
+export default App;
