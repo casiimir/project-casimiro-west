8d77e826b6261c772a5d4d6f30981bfa943779f5
Marcoviscu <marcoviscuso2000@gmail.com>
2022-09-29 20:01:04 +0200

    feat:addedd Activities page component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,15 @@
-
 import ActivitiesHomeList from "../../components/ActivitiesHomeList";
 import Hero from "../../components/Hero";
+import TopCityCardList from "../../components/TopCityCardList/TopCityCardList";
 import styles from "./index.module.scss";
 
 const Home = () => {
   return (
     <div className={styles.Home}>
-      <Hero/>
-     
+      <Hero />
+      <TopCityCardList />
     </div>
   );
 };
 
-
-export default Home
+export default Home;
