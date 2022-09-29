cb3af9c6c975537bbb919e0035bac0c7d2fd1dd8
Federica129 <schillacifederica99@gmail.com>
2022-09-30 01:42:36 +0200

    fix/style-in-general

--- src/components/TopCityCardList/TopCityCardList.jsx
+++ src/components/TopCityCardList/TopCityCardList.jsx
@@ -1,9 +1,9 @@
-
 import styles from "./index.module.scss";
 import CityCard from "../CityCard";
 import { GET } from "../../utils/api";
 import { useDispatch, useSelector } from "react-redux";
-import { memo, useEffect, useRef } from "react";
+import React, { memo, useEffect, useRef } from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 const TopCityCardList = () => {
   const cityData = useSelector((state) => state.cities);
@@ -12,25 +12,37 @@ const TopCityCardList = () => {
   const containerRef = useRef(null);
 
   const next = () => {
-    containerRef.current.scrollLeft += 200
+    containerRef.current.scrollLeft += 278;
   };
   const prev = () => {
-    containerRef.current.scrollLeft -= 200
+    containerRef.current.scrollLeft -= 278;
   };
 
   const dispatch = useDispatch();
   useEffect(() => {
     GET("cities").then((data) =>
-      dispatch({ type: "SET_CITIES_DATA", payload:data.filter((el) => el.show_in_popular === true) })
+      dispatch({
+        type: "SET_CITIES_DATA",
+        payload: data.filter((el) => el.show_in_popular === true),
+      })
     );
   }, [dispatch]);
 
-  return (<><button className={styles.button} onClick={prev}>-</button>
-      <div  ref={containerRef} className={styles.TopCityCardList}>
-        {cityData?.data?.map((el)=> <CityCard key={el.id} CardData={el}/>)}
-      </div><button className={styles.button} onClick={next}>+</button></>
-  )
+  return (
+    <div className={styles.box}>
+      <button className={styles.button} onClick={prev}>
+        <IoIosArrowBack />
+      </button>
+      <div ref={containerRef} className={styles.TopCityCardList}>
+        {cityData?.data?.map((el) => (
+          <CityCard key={el.id} CardData={el} />
+        ))}
+      </div>
+      <button className={styles.button} onClick={next}>
+        <IoIosArrowForward />
+      </button>
+    </div>
+  );
 };
 
 export default memo(TopCityCardList);
-
