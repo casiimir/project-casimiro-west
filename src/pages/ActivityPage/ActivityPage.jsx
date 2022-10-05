1f28d27c1ec3c4e845978be0c211a35517818bf8
LoreMaru <gialma1120@hotmail.it>
2022-10-05 12:56:05 +0200

    Feat:button-animation

--- src/pages/ActivityPage/ActivityPage.jsx
+++ src/pages/ActivityPage/ActivityPage.jsx
@@ -2,6 +2,7 @@ import { Outlet, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { GiPositionMarker } from "react-icons/gi";
+import { FaShoppingCart, FaHeart } from "react-icons/fa";
 import { TbCurrencyDollar } from "react-icons/tb";
 import Map from "../../components/Map";
 import styles from "./index.module.scss";
@@ -11,8 +12,14 @@ import { isEditable } from "@testing-library/user-event/dist/utils";
 import { style } from "@mui/system";
 
 const ActivityPage = () => {
-  const [cartAnimation, setCartAnimation] = useState("");
-
+  const [animation, setAnimation] = useState({
+    buttonStyle: "",
+    button: `${styles.button2}`,
+    addText: `${styles.add}`,
+    thanksText: `${styles.thanks}`,
+    cartIcon: `${styles.cart}`,
+    heartIcon: `${styles.heart}`,
+  });
   const data = useLocation();
   const activityData = useSelector((state) => state.SingleActivity);
 
@@ -30,7 +37,7 @@ const ActivityPage = () => {
     retail_price,
   } = data.state;
 
-  const testObject = {
+  const cartObject = {
     name: `${title}`,
     IMG: `${cover_image_url}`,
     price: `${retail_price.value}`,
@@ -40,10 +47,28 @@ const ActivityPage = () => {
     dispatch({
       type: "SET_CART_DATA",
       payload: [
-        localStorage.setItem(`${title}@@@`, JSON.stringify(testObject)),
+        localStorage.setItem(`${title}@@@`, JSON.stringify(cartObject)),
       ],
     });
-    setCartAnimation(`${styles.clicked}`);
+
+    setAnimation({
+      buttonStyle: "white",
+      button: `${styles.button2}`,
+      addText: `${styles.hidden}`,
+      thanksText: `${styles.thanksActive}`,
+      cartIcon: `${styles.cartActive}`,
+      heartIcon: `${styles.heartActive}`,
+    });
+    setTimeout(() => {
+      setAnimation({
+        buttonStyle: "",
+        button: `${styles.button2}`,
+        addText: `${styles.add}`,
+        thanksText: `${styles.thanks}`,
+        cartIcon: `${styles.cart}`,
+        heartIcon: `${styles.heart}`,
+      });
+    }, 3000);
   };
 
   const imgFormatter = (URL, FILTER) => {
@@ -73,17 +98,19 @@ const ActivityPage = () => {
               <p>{city.name}</p>
             </div>
           </div>
-          <div className={styles.priceInfo}>
-            <span>{/* <TbCurrencyDollar /> {retail_price.value} */}</span>
-            <button onClick={cartFunction}>
-              {" "}
-              Add to cart <br /> ${retail_price.value}
-            </button>
-            <button onClick={cartFunction}>
-              {" "}
-              Add to cart ${retail_price.value}
-            </button>
-          </div>
+
+          <button
+            onClick={cartFunction}
+            className={animation.button}
+            style={{ backgroundColor: `${animation.buttonStyle}` }}
+          >
+            <FaHeart className={animation.heartIcon} />
+            <FaShoppingCart className={animation.cartIcon} />
+            <p className={animation.addText}>
+              Add to Cart ${retail_price.value}
+            </p>
+            <p className={animation.thanksText}>Thank You!</p>
+          </button>
         </div>
         <div className={styles.info}>
           <p>{about}</p>
@@ -96,8 +123,6 @@ const ActivityPage = () => {
               </p>
 
               <div>
-                {/* <div className={styles.mapOverlayLeft}></div>
-            <div className={styles.mapOverlayRight}></div> */}
                 <div className={styles.mapDisplay}>
                   <Map lng={longitude} lat={latitude} />
                 </div>
@@ -112,40 +137,3 @@ const ActivityPage = () => {
 };
 
 export default ActivityPage;
-//<TbCurrencyDollar />
-// 159430   85422   148584
-// { PRENDERE I DATI
-//   Object.values(localStorage)
-//     .filter((e) => e.includes("name"))
-//     .map((item) => JSON.parse(item))
-//     .map((item, index) => (
-//       <div key={index}>
-//         <img src={item.IMG} /> <p>{item.name}</p>
-//         <p>{item.price}</p>{" "}
-//       </div>
-//     ));
-// }
-
-//FARE LA SOMMA (ALLA FINE MOSTRARE TOTAL)
-// const [total, setTotal] = useState([]);
-// const accumulatore = [];
-
-// useEffect(() => {
-//   Object.values(localStorage)
-//     .filter((e) => e.includes("name"))
-//     .map((item) => JSON.parse(item))
-//     .map((item, index) => accumulatore.push(Number(item.price)));
-//   setTotal(
-//     accumulatore.reduce((previous, next) => {
-//       return previous + next;
-//     })
-//   );
-// });
-
-//FORMULA MAGICA IMMAGINE
-//  const imgFormatter = (URL, FILTER) => {
-//   const original = URL.substring(0, URL.length - 6);
-//   return `${original}${FILTER}`;
-// };
-// PRENDERE IMMAGINE
-// src={imgFormatter(`${cover_image_url}`, "?w=1000")} <- METTERE LE DIMENSIONI DESIDERATE E/O QUALUNQUE ALTRO FILTR IMGIX
