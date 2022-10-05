ba1d1ddb1201b45fff0abbf59036a816db31a12f
Marcoviscu <marcoviscuso2000@gmail.com>
2022-10-05 19:48:17 +0200

    feat:cart logic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,16 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./index.module.scss";
 import logo from "./logo.png";
 import logotypeW from "./logo_white.png";
 import logoBlack from "./logotype_white.png";
-import MainInput from "../MainInput/";
 import { useRef } from "react";
 import { Link } from "react-router-dom";
 import { RiMenu3Line } from "react-icons/ri";
 import { FaShoppingCart } from "react-icons/fa";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 
-const Header = ({ children }) => {
+const Header = ({ children, cartNumber, setCartNumber }) => {
   const dispatch = useDispatch();
   const [active, setActive] = useState();
 
@@ -22,8 +21,6 @@ const Header = ({ children }) => {
       payload: setActive(styles.active),
     });
   }, []);
-
-  const { cartData } = useSelector((state) => state.cart);
 
   const navRef = useRef(null);
 
@@ -61,28 +58,14 @@ const Header = ({ children }) => {
           </ul>
         </div>
         <section className={styles.side}>
-          <MainInput />
+          {children}
           <div className={styles.menu} onClick={onHadleClick}>
             <RiMenu3Line />
           </div>
           <Link to="/cart">
             <div className={styles.cart}>
               <FaShoppingCart />
-              {localStorage.length > 2 ? (
-                cartData ? (
-                  <p className={styles.number}>{cartData.length}</p>
-                ) : (
-                  <p className={styles.number}>
-                    {
-                      Object.keys(localStorage).filter(
-                        (e) => !e.includes("mapbox")
-                      ).length
-                    }
-                  </p>
-                )
-              ) : (
-                ""
-              )}
+              <p>{cartNumber}</p>
             </div>
           </Link>
         </section>
@@ -146,16 +129,14 @@ const Header = ({ children }) => {
             About Us
           </Link>
         </ul>
-        <div className={styles.button}>
-          <button>
-            <span>Cart</span>
+        <Link to="/cart">
+          <div className={styles.button}>
+            <button>
+              <span>Cart</span>
 
-            <span>
-              <FaShoppingCart />
-              {localStorage.length > 2 ? (
-                cartData ? (
-                  <p>{cartData.length}</p>
-                ) : (
+              <span>
+                <FaShoppingCart />
+                {localStorage.length > 2 ? (
                   <p>
                     {
                       Object.keys(localStorage).filter(
@@ -163,13 +144,13 @@ const Header = ({ children }) => {
                       ).length
                     }
                   </p>
-                )
-              ) : (
-                ""
-              )}
-            </span>
-          </button>
-        </div>
+                ) : (
+                  ""
+                )}
+              </span>
+            </button>
+          </div>
+        </Link>
       </div>
     </header>
   );
