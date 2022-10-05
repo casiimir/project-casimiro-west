e4c7aed67c37ac41cf277a8a138a9269961e0f8e
martina-lv <livigni.martina@gmail.com>
2022-10-05 21:21:12 +0200

    feat:fixed-images-titles-in-activities

--- src/components/CardActivity/CardActivity.jsx
+++ src/components/CardActivity/CardActivity.jsx
@@ -14,9 +14,10 @@ const CardActivity = ({ data }) => {
       payload: data,
     });
   };
+  console.log("DATA ====", data.cover_image_url);
 
   return (
-  <>
+    <>
       <Link
         to={`/activity/${data.title}`}
         state={data}
@@ -24,15 +25,15 @@ const CardActivity = ({ data }) => {
         onClick={() => window.scrollTo(0, 0)}
       >
         <div className={styles.CardActivity} onClick={setActivityFunction}>
-          {data.city.cover_image_url !== "" ? (
-        <img
-          className={styles.photo}
-          src={`${data?.city.cover_image_url}?w=300`}
-          alt="img"
-        />
-      ) : (
-        <img className={styles.photo} src={placeholder} alt="img" />
-      )}
+          {data.cover_image_url !== "" ? (
+            <img
+              className={styles.photo}
+              src={`${data?.cover_image_url}?w=300`}
+              alt="img"
+            />
+          ) : (
+            <img className={styles.photo} src={placeholder} alt="img" />
+          )}
           <section className={styles.tourInfo}>
             <h2 className={styles.name}>{data?.title}</h2>
             <div className={styles.description}>
